72446ffbc3ad5eb2e477081d61ceb703c82dc2fb
albabernal03 <91721875+albabernal03@users.noreply.github.com>
2024-02-08 16:14:41 +0100

    instalamos las librerias necesarias

--- analisis.py
+++ analisis.py
@@ -1,6 +1,2 @@
-import pandas as pd
-
-#Lo que vamos hacer es extraer las tablas del grupo A al grupo H de la liga de campeones de la UEFA 2023-24
-all_tables = pd.read_html('https://es.wikipedia.org/wiki/Liga_de_Campeones_de_la_UEFA_2023-24')
-
-all_tables
+import requests
+from bs4 import BeautifulSoup
