fb30d9df899d89180f9aaac7f246088b9a995116
albabernal03 <91721875+albabernal03@users.noreply.github.com>
2024-02-15 16:25:09 +0100

    hemos conseguido scrapear

--- analisis.py
+++ analisis.py
@@ -1,11 +1,35 @@
-
 from bs4 import BeautifulSoup
 import requests
-years=[2013,2014,2015,2016,2017,2018,2019,2020,2021,2022]
-web='https://espndeportes.espn.com/futbol/posiciones/_/liga/UEFA.CHAMPIONS/ordenar/pointdifferential/dir/desc/temporada/2022'
-response=requests.get(web)
-print(response.text)
-'''soup=BeautifulSoup(content, 'lxml')'''
+import pandas as pd
+
+# URL de la página web
+url = 'https://es.wikipedia.org/wiki/Anexo:Octavos_de_final_de_la_Liga_de_Campeones_de_la_UEFA_2022-23'
+
+response = requests.get(url)
+soup = BeautifulSoup(response.text, 'html.parser')
+
+# Buscamos la tabla de la página web
+tabla = soup.find('table', {'class': 'wikitable'})
+
+# Verificamos si la tabla existe
+if tabla:
+    data = []
+    rows = tabla.find_all('tr')
+    for row in rows:
+        cols = row.find_all(['th', 'td'])  # Buscar tanto en celdas de encabezado como en celdas de datos
+        row_data = [cell.get_text(strip=True) for cell in cols]
+        data.append(row_data)
+
+    # Convertir a DataFrame de pandas
+    df = pd.DataFrame(data)
+
+    # Guardar en un archivo CSV
+    df.to_csv('datos_octavos_champions.csv', index=False)
+
+    print("Datos guardados exitosamente en 'datos_octavos_champions.csv'.")
+else:
+    print("No se encontró la tabla en la página.")
+
 
 
 
