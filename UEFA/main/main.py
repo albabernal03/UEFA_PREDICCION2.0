30549af941cf8177a1364adb5b37ad25622771ff
albabernal03 <91721875+albabernal03@users.noreply.github.com>
2024-05-04 18:05:36 +0200

    borramos las pruebas que hicimos del main

--- UEFA/main/main.py
+++ UEFA/main/main.py
@@ -7,7 +7,6 @@ from nbformat import read
 # Definir las rutas de los archivos de datos
 rutas_datos = {
     "partidos": "../../data/partidos_limpio.csv",
-    "partidos_2023-2024": "../../data/partidos_2023-2024.csv",
     "equipos": "../../data/equipos.csv",
     "jugadores": "../../data/jugadores.csv",
     # Agrega más rutas de archivos según sea necesario
@@ -24,36 +23,21 @@ modelos_indices_celda = {
 # Función para ejecutar un notebook y devolver el resultado
 def ejecutar_notebook(notebook, indice_celda):
     try:
-        # Ejecutar el notebook y guardar el resultado en un archivo temporal
-        output_path = notebook[:-6] + "_output.ipynb"
-        pm.execute_notebook(
+        # Ejecutar el notebook sin guardar la salida en un archivo temporal
+        output_nb = pm.execute_notebook(
             notebook,
-            output_path,
+            None,  # No se guarda la salida en ningún archivo
             parameters={"rutas_datos": rutas_datos},  # Pasar el diccionario de rutas de archivos como parámetro
             kernel_name='python'
         )
 
-        # Devolver la ruta del archivo de salida donde se guarda el resultado
-        return output_path
+        # Obtener el resultado de la ejecución de la celda de salida
+        output_resultado = output_nb.cells[indice_celda].outputs[0].data['text/plain']
+        
+        # Devolver el resultado
+        return output_resultado
     except Exception as e:
         print(f"Error al ejecutar el notebook {notebook}: {str(e)}")
-        return None
-
-# Función para obtener el resultado de la ejecución de la celda de salida
-def obtener_output(output_notebook, indice_celda):
-    try:
-        # Leer el resultado del notebook de salida
-        with open(output_notebook, 'r') as f:
-            nb_output = read(f, 4)
-
-        # Obtener el contenido de la celda de salida asociada al índice
-        if nb_output.cells and indice_celda < len(nb_output.cells):
-            output_source = nb_output.cells[indice_celda].outputs[0].data['text/plain']
-            return output_source
-        else:
-            return None
-    except Exception as e:
-        print(f"Error al obtener el output del notebook {output_notebook}: {str(e)}")
         return None
 
 # Carpeta raíz donde se encuentran los modelos
@@ -89,19 +73,14 @@ if opcion.isdigit() and 1 <= int(opcion) <= len(notebooks_encontrados):
     indice_celda = modelos_indices_celda.get(os.path.abspath(notebook_seleccionado))
     
     if indice_celda is not None:
-        # Ejecutar el notebook
-        output_notebook = ejecutar_notebook(notebook_seleccionado, indice_celda)
-        if output_notebook:
-            # Obtener el resultado de la ejecución de la celda de salida
-            output_resultado = obtener_output(output_notebook, indice_celda)
-            if output_resultado:
-                print("Resultado de la ejecución:")
-                print(output_resultado)
-            else:
-                print("No se encontró ningún resultado de ejecución para la celda de salida.")
+        # Ejecutar el notebook y obtener el resultado
+        output_resultado = ejecutar_notebook(notebook_seleccionado, indice_celda)
+        if output_resultado:
+            print("Resultado de la ejecución:")
+            print(output_resultado)
+        else:
+            print("No se encontró ningún resultado de ejecución para la celda de salida.")
     else:
         print("No se ha especificado un índice de celda para este modelo.")
 else:
-    print("Opción inválida.")
-
     print("Opción inválida.")
